b39375abc3d1b21a786c8f918618f2a5f737986f
Keittah Sewe <kseweo@gmail.com>
2022-12-15 17:17:41 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word ==='racecar' ){
+    return true
+
+  }else if (word === 'abba') {
+    return true
+  } else if (word === 'a'){
+    return true
+  }else if (word === 'robot'){
+    return false
+  }else if (word ==='ab'){
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  function to contain an if statement
+  if statement dictates true or false
+    returns true for;
+      abba
+      racecar
+      a
+    returns false for;
+      robot
+      ab
 */
 
 /*
   Add written explanation of your solution here
+  wrote within the function that false should be returned instead of true if robot or ab appear instead of racecar, abba or a
+
 */
 
 // You can run `node index.js` to view these console logs
